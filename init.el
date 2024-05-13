07dbab7711f868cd564aab020c4e9ffc21a9a7c2
Ruan Pasta <ruan.pasta@exactaworks.com.br>
2024-05-13 09:38:35 -0300

    doc: Included the menu

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 '(magit ob-javascript org-bullets command-log-mode prettier scss-mode flycheck web-mode svelte-mode vue-mode json-mode projectile doom-themes doom-modeline counsel company typescript-mode which-key dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui lsp-mode ivy)))
+	 '(toc-org magit ob-javascript org-bullets command-log-mode prettier scss-mode flycheck web-mode svelte-mode vue-mode json-mode projectile doom-themes doom-modeline counsel company typescript-mode which-key dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui lsp-mode ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
