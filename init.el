081f21092cf767dc0e25dd666e1d01eea48f57c4
Ruan Augusto Pasta <ruan@pasta.0.0.179>
2024-08-28 23:21:10 -0300

    Updating the emacs config with EXWM

--- init.el
+++ init.el
@@ -1,17 +1,15 @@
-
 (require 'org)
 (require 'ob-tangle)
 
 ;;; Code:
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
-  
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 '(evil toc-org magit ob-javascript org-bullets command-log-mode prettier scss-mode flycheck web-mode svelte-mode vue-mode json-mode projectile doom-themes doom-modeline counsel company typescript-mode which-key dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui lsp-mode ivy)))
+	 '(toc-org org-bullets command-log-mode dap-mode magit prettier flycheck scss-mode web-mode svelte-mode vue-mode json-mode typescript-mode which-key company lsp-ui lsp-mode projectile doom-modeline counsel ivy doom-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
