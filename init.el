c53649016c61db12ee5b88b3cf16a1706e4db65b
ruanpasta <ruan_augusto_pasta@hotmail.com>
2024-09-04 10:12:39 -0300

    Updating the emacs config before the refactor

--- init.el
+++ init.el
@@ -1,7 +1,10 @@
+;;; package --- Summary
 (require 'org)
 (require 'ob-tangle)
 
 ;;; Code:
+
+;;; Commentary:
 
 ;; Initial load
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
@@ -14,7 +17,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 '(toc-org org-bullets command-log-mode dap-mode magit prettier flycheck scss-mode web-mode svelte-mode vue-mode json-mode typescript-mode which-key company lsp-ui lsp-mode projectile doom-modeline counsel ivy doom-themes use-package)))
+	 '(exec-path-from-shell toc-org org-bullets command-log-mode dap-mode magit prettier flycheck scss-mode web-mode svelte-mode vue-mode json-mode typescript-mode which-key company lsp-ui lsp-mode projectile doom-modeline counsel ivy doom-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
