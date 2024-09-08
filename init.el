2c62a3971f883a4aae29d5f97efad145abe7e9f6
ruanpasta <ruan_augusto_pasta@hotmail.com>
2024-09-08 15:20:52 -0300

    feat: New version of emacs config for Linux
    In this version I fixed some bugs related a language servers
    auto-completion in a Linux environment.

--- init.el
+++ init.el
@@ -17,7 +17,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 '(exec-path-from-shell toc-org org-bullets command-log-mode dap-mode magit prettier flycheck scss-mode web-mode svelte-mode vue-mode json-mode typescript-mode which-key company lsp-ui lsp-mode projectile doom-modeline counsel ivy doom-themes use-package)))
+	 '(auto-package-update rainbow-delimiters company-box lsp-ivy ivy-prescient ivy-rich exec-path-from-shell toc-org org-bullets command-log-mode dap-mode magit prettier flycheck scss-mode web-mode svelte-mode vue-mode json-mode typescript-mode which-key company lsp-ui lsp-mode projectile doom-modeline counsel ivy doom-themes use-package))
+ '(warning-suppress-types '((lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
