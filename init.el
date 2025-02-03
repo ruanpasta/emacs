dcbb463c9e8e84e9b5bee2aa67e1fc64e96ddb8b
ruanpasta <ruan_augusto_pasta@hotmail.com>
2025-02-03 19:11:56 -0300

    config: Adding the Clojure into my config

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-	 '(auto-package-update rainbow-delimiters company-box lsp-ivy ivy-prescient ivy-rich exec-path-from-shell toc-org org-bullets command-log-mode dap-mode magit prettier flycheck scss-mode web-mode svelte-mode vue-mode json-mode typescript-mode which-key company lsp-ui lsp-mode projectile doom-modeline counsel ivy doom-themes use-package))
+	 '(cider clojure-mode auto-package-update rainbow-delimiters company-box lsp-ivy ivy-prescient ivy-rich exec-path-from-shell toc-org org-bullets command-log-mode dap-mode magit prettier flycheck scss-mode web-mode svelte-mode vue-mode json-mode typescript-mode which-key company lsp-ui lsp-mode projectile doom-modeline counsel ivy doom-themes use-package))
  '(warning-suppress-types '((lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
